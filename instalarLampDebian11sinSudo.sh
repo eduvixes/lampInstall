c28a180e4e82a6ecd88254aa7a831b172c3bdb5a
eduvixes <jrodeiro@gmail.com>
2022-12-15 16:28:01 +0100

    Update instalarLampDebian11sinSudo.sh

--- instalarLampDebian11sinSudo.sh
+++ instalarLampDebian11sinSudo.sh
@@ -5,9 +5,6 @@
 # actualizamos el sistema.
 apt update && apt -y upgrade
 
-# instalamos servidor ssh
-apt install openssh-server
-
 # instalamos ssh
 apt install ssh
 
@@ -74,42 +71,6 @@ systemctl restart apache2
 
 wget "http://www.adminer.org/latest.php" -O /var/www/html/adminer.php
 chown www-data:www-data /var/www/html/adminer.php
-
-# instalar phpmyadmin
-
-wget https://files.phpmyadmin.net/phpMyAdmin/5.1.1/phpMyAdmin-5.1.1-all-languages.tar.gz
-tar xvf phpMyAdmin-5.1.1-all-languages.tar.gz
-mv phpMyAdmin-5.1.1-all-languages /usr/share/phpmyadmin
-mkdir -p /var/lib/phpmyadmin/tmp
-chown -R www-data:www-data /var/lib/phpmyadmin
-cp /root/lampInstall/config.inc.php /usr/share/phpmyadmin/config.inc.php
-
-apt install pwgen
-pwgen -s 32 1
-
-cp /root/lampInstall/phpmyadmin.conf /etc/apache2/conf-available/phpmyadmin.conf
-
-## ===========================================================================================
-
-/sbin/a2enconf phpmyadmin.conf
-
-# crear usuario pma de gestion del phpmyadmin
-echo "create user pma@localhost identified by 'pmaphpmyadmin';" > sqltemp.sql
-echo "grant all privileges on phpmyadmin.* to pma@localhost;" > sqltemp.sql
-echo "FLUSH PRIVILEGES;" >> sqltemp.sql
-echo "quit;" >> sqltemp.sql
-
-mysql -h localhost -u root < sqltemp.sql
-rm sqltemp.sql
-
-# instalar git
-
-apt install git
-
-# añadir usuario base
-
-/usr/sbin/adduser invitado 
-# poner contraseña 'invitado'
 
 # actualices de nuevo todo el sistema.
 apt update && apt -y upgrade
